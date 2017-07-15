dc1d6a338fec93e6078015a70201613986812771
James W. Tonkin <btonkin71@comcast.net>
2017-07-15 15:52:55 -0500

    Start of Staff Class, left off at Methods.

--- CSPayrollProject/CSPayrollProject/Program.cs
+++ CSPayrollProject/CSPayrollProject/Program.cs
@@ -4,8 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSPayrollProject
+namespace CSProject
 {
+    class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        public float TotalPay() { get; protected set; }
+
+        public float BasicPay() { get; private set; }
+
+        public string NameOfStaff() { get; private set; }
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        public Staff(string name, float rate)
+        // Parent constructor for Staff class
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
