50fa848e23341fbfcebf69bb584c8b476615ae2b
James W. Tonkin <btonkin71@comcast.net>
2017-07-24 09:16:40 -0500

    created the Admin class

--- CSPayrollProject/CSPayrollProject/Program.cs
+++ CSPayrollProject/CSPayrollProject/Program.cs
@@ -76,6 +76,29 @@ namespace CSProject
         }
     }
 
+    class Admin : Staff
+    {
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base (name, adminHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if (HoursWorked > 160)
+                Overtime = overtimeRate * (HoursWorked - 160);
+        }
+
+        public override string ToString()
+        {
+            return "\nNameofStaff: " + NameOfStaff + "\nadminHourlyRate: " + adminHourlyRate + "\novertimeRate: " + overtimeRate + "\nHoursWorked: " + HoursWorked + "\nBasicPay: " + BasicPay + "\nTotalPay: " + TotalPay;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
